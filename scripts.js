d9f3703e3e7655de4f3bd736cb98e9b27db60fbf
Francisco Macedo <macedo270701@gmail.com>
2024-10-09 14:17:20 -0300

    inicialização do repo

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Função de scroll suave ao clicar nos links do menu
+document.querySelectorAll("nav ul li a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scroll({
+        top: targetElement.offsetTop - 20,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+// Função para mudar o cabeçalho ao rolar a página
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 100) {
+    header.style.backgroundColor = "#005f8a";
+  } else {
+    header.style.backgroundColor = "#0077b6";
+  }
+});
